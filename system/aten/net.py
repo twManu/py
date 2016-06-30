2bb86e6a453c6018cdd8570271dbff7dc2b533d6
Manu <Manu>
2016-06-30 19:59:06 +0800

    feat: update

--- system/aten/net.py
+++ system/aten/net.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, re
+import sys, re, os
 import argparse
 
 sys.path.append('../../basic')
@@ -38,18 +38,32 @@ class routing(manuLib):
 	#       None - means print routing rules
 	# Ret : True - existing
 	#       False - otherwise
-	def ruleExisting(self, netRule=None):
+	def _ruleExisting(self, netRule=None):
 		cmpStr=""
 		if netRule: cmpStr=self._ruleStr(netRule)
 		for curRule in self.cmdOutput('ip route'):
 			if cmpStr:
-				if re.match(cmpStr, curRule):
+				word0=curRule.split()[0]
+				if cmpStr==word0:
 					return True
 			else:
 				line=curRule.strip()
 				if line:
 					print line
 		return False
+
+
+	# Check if the ipMsk already exists
+	#
+	# In  : ipMsk - a.b.c.d or a.b.c.d/n
+	# Ret : True - existing
+	#       False - otherwise
+	def _ipMskStrExisting(self, ipMsk):
+		for curRule in self.cmdOutput('ip route'):
+			word0=curRule.split()[0]
+			if ipMsk==word0: return True
+		return False
+
 
 	# add or delete rules routing internally
 	# In  : onoff - True : add
@@ -59,19 +73,34 @@ class routing(manuLib):
 			return
 		for rule in INT_NET:
 			ipMsk=self._ruleStr(rule)
+			exist=self._ruleExisting(rule)
 			if onoff:                #add
-				if self.ruleExisting(rule):
+				if exist:
 					print ipMsk+" already exists ...ignore"
 					continue
 				cmd = "ip route add "+ipMsk
 				cmd += " dev "+self._intf+" via "+gw
-				os.system(cmd)
 			else:
-				if not self.ruleExisting(rule):
+				if not exist:
 					print ipMsk+" not in routing table"
 					continue
 				cmd = "ip route del "+ipMsk
-				os.system(cmd)
+			os.system(cmd)
+
+
+	# delete a routing rule
+	# In  : ipMsk - a.b.c.d/maskLen or a.b.c.d
+	def delRoute(self, ipMsk):
+		if not self. _ipMskStrExisting(ipMsk):
+			print ipMsk+' doesn\'t exist in rule'
+		cmd = "ip route del "+ipMsk
+		os.system(cmd)
+
+
+	# print all routing info	
+	def showRoute(self):	
+		self._ruleExisting()
+		
 
 
 #
@@ -82,10 +111,12 @@ if __name__ == '__main__':
 		parser=argparse.ArgumentParser()
 		parser.add_argument('-s', action='store_true', dest='show_route', default=False,
 			help='show routings')
-		parser.add_argument('-i', action='store', dest='internal_on', default='',
-			help='0: delete rules, 1: add rules')
+		parser.add_argument('-i', action='store', dest='internal_on', default=0,
+			help='1: add rules, otherwise: del rules')
 		parser.add_argument('-d', action='store', dest='interface', default=INT_INTF,
 			help='the network interface to do with')
+		parser.add_argument('-D', action='store', dest='del_net', default="",
+			help='the network to delete')
 		arg = parser.parse_args()
 		return arg
 
@@ -93,7 +124,7 @@ if __name__ == '__main__':
 	if arg.show_route:
 		route=routing()
 		#print rules
-		route.ruleExisting()
+		route.showRoute()
 		sys.exit(0)
 
 	route=routing(arg.interface)
@@ -102,5 +133,8 @@ if __name__ == '__main__':
 		sys.exit(1)
 	#do according to internal table
 	if arg.internal_on:
-		route.intRoute(1==arg.internal_on)
+		print "value is "+arg.internal_on
+		route.intRoute('1'==arg.internal_on)
+	elif arg.del_net:
+		route.delRoute(arg.del_net)
 
