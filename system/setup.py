ec7d6bd9e0571d2b0f07e30750ae0f1ea839ea04
manuchen <manuchen@gmail.com>
2018-01-21 11:06:25 +0800

    update

--- system/setup.py
+++ system/setup.py
@@ -7,9 +7,9 @@ from lib import manuLib
 
 
 class packet(object):
-	PKT_BASIC="vim ssh synaptic dos2unix git"
-	PKT_DEV="g++ build-essential gitk meld minicom"
-	PKT_DEVNET="tftpd-hpa nfs-common nfs-kernel-server"
+	PKT_BASIC="vim ssh synaptic dos2unix gitk"
+	PKT_DEV="g++ build-essential meld minicom libpython-dev"
+	PKT_DEVNET="tftpd-hpa nfs-common nfs-kernel-server openssh-server"
 	PKT_NET="filezilla"
 	PKT_FCITX="cmake fcitx-libs-dev fcitx-tools fcitx-table"
 	ALL_PKT=(PKT_BASIC, PKT_DEV, PKT_DEVNET, PKT_NET, PKT_FCITX)
@@ -20,9 +20,9 @@ class packet(object):
 class apt(object):
 	def __init__(self):
 		self._lib=manuLib()
-		if not self._lib.isRoot():
-			print "Installer must be run as root"
-			sys.exit(1)
+		self._isRoot=False
+		if self._lib.isRoot():
+			self._isRoot=True
 		self._pkt=packet() 
 
 	# Return interface dictionary and optionally print the message
@@ -38,14 +38,14 @@ class apt(object):
 if __name__ == '__main__':
 	def check_param():
         	parser = argparse.ArgumentParser()
-	        parser.add_argument('-i', action='store_true', dest='info', default=False,
-        	        help='information')
+	        parser.add_argument('-q', action='store_true', dest='query', default=False,
+        	        help='query package and installation')
 	        arg=parser.parse_args()
         	return arg
 
 	arg=check_param()
 	obj=apt()
-	if arg.info:
+	if arg.query:
 		obj.getInfo()
 	else:
 		obj.getInfo()
