52d3798daa9501d12e323ce59ac275fe0dcc609e
ManuChen <manuchen@gmail.com>
2018-02-15 19:15:46 +0800

    feat: such BIG-5 doesn't work with Win10

--- basic/encoding/field.py
+++ basic/encoding/field.py
@@ -15,18 +15,22 @@ class field(object):
 		self._desc = fieldDesc
 		self._index = index
 		self._name = name
+		knownType = {
+			  'b': 1
+			, 'B': 1
+			, 'h': 2
+			, 'H': 2
+			, 'i': 4
+			, 'I': 4
+		}
 		for ff in fieldDesc['fields']:
-			if fieldDesc[ff] == 'b' or fieldDesc[ff] == 'B':
-				#self._field[ff], = struct.unpack(fieldDesc[ff], f.read(1))
-				self._field[ff] = struct.unpack(fieldDesc[ff], f.read(1))[0]
-			elif fieldDesc[ff] == 'h' or fieldDesc[ff] == 'H':
-				#self._field[ff], = struct.unpack(fieldDesc[ff], f.read(2))
-				self._field[ff] = struct.unpack(fieldDesc[ff], f.read(2))[0]
-			elif fieldDesc[ff] == 'i' or fieldDesc[ff] == 'I':
-				#self._field[ff], = struct.unpack(fieldDesc[ff], f.read(4))
-				self._field[ff] = struct.unpack(fieldDesc[ff], f.read(4))[0]
+			if fieldDesc[ff] in knownType:
+				print 'to read for', ff, knownType[fieldDesc[ff]], 'bytes'
+				self._field[ff] = struct.unpack(fieldDesc[ff],\
+					f.read(knownType[fieldDesc[ff]]))[0]
 			else:
 				raise NameError('unknown field type')
+
 
 	#given key, return value if valid
 	#otherwise none returned
@@ -35,19 +39,23 @@ class field(object):
 			return self._field[key]
 		return None
 
+
 	#set value to key
 	def set(self, key, value):
 		if self._field.has_key(key):
 			self._field[key] = value
+
 
 	#reverse the unpack procedure
 	def write(self, f):
 		for ff in self._desc['fields']:
 			f.write(struct.pack(self._desc[ff], self._field[ff]))
 
+
 	def show(self):
 		for key, value in self._field.iteritems():
 			print key+': '+str(value)
+
 
 	def setName(self, name):
 		self._name = name
@@ -67,11 +75,18 @@ class field(object):
 
 #main
 if __name__ == '__main__':
-	fDesc = {
+'''
+	doesn't work w/ Windows
 		'fields': ('忠義', '仁愛', '勇氣'),
 		'忠義': 'B',
 		'仁愛': 'B',
 		'勇氣': 'B'
+'''
+	fDesc = {
+		'fields': ('loyalty', 'mercy', 'courage'),
+		'loyalty': 'B',
+		'mercy': 'B',
+		'courage': 'B'
 	} 
 	with open('test', "rb") as f:
 		#pop offset 16 and '年紀', '國家', '地區', '體力', '體力上限'
