2fee3efdfe1bedd700d4323797704e1e3da28897
manu <manu>
2019-08-04 14:18:58 +0800

    feat: cooking y4m

--- v4l2/y4m.py
+++ v4l2/y4m.py
@@ -2,37 +2,104 @@
 
 import os, sys, struct, argparse, re
 
-file="/media/manu/230g/elephants_dream_1080p24.y4m"
+g_d4File="/tmp/save000.y4m"
 
-def pureNumInStr(inputStr):
-    return re.sub(r'[^0-9]', "", inputStr)
+class cY4M:
+    def __init__(self, args):
+        self._args = None
+        self._inFile = None
+        self._size = 0
+        if args:
+            self._args = args
+            self._inFile = args.inFile
+        if not self._inFile:
+            self._inFile = g_d4File
+        if not os.path.isfile(self._inFile):
+            print "invalid input file", self._inFile
+            self._inFile = ""
 
-with open(file, 'r') as fr:
-    line_nr=0
-    for line in fr:
-        line_nr += 1
-        if 1==line_nr:
+
+    #get identification and value
+    def pureNumInStr(self, inputStr):
+        matches = {
+            'W': r'W([0-9]+)',
+            'H': r'H([0-9]+)',
+            'A': r'A([^A]+)',
+            'I': r'I[^I]',
+            'F': r'F([^F]+)'
+        }
+        for key in matches:
+            match = re.match(matches[key], inputStr)
+            if match:
+                return key, match
+        return '', 0
+
+
+    def levelPnt(self, level, msg):
+        if level>=self._args.verbose:
+            print msg
+            return True
+        return False
+
+    
+    def parse(self):
+        if not self._inFile:
+            return
+        with open(self._inFile, 'r') as fr:
+            #1st line
+            line = fr.readline()
+            self.levelPnt(2, line)
             words = line.split()
             print "format=", words[0]
             if words[0]!="YUV4MPEG2":
                 print "error file format"
-                break
-            print "encoding=", words[1]
-            #dimension
-            _width=pureNumInStr(words[2])
-            _height=pureNumInStr(words[3])
-            print "dimension=", _width, _height
-            #fps
-            fpss=words[4].split(':')
-            _fps1=pureNumInStr(fpss[0])
-            _fps2=pureNumInStr(fpss[1])
-            print "fps=", float(_fps1)/float(_fps2)
-            print "ratio=", words[5]
-        elif 2==line_nr:
-            #FRAME
-            print line
-        else:
-            frame=re.sub(r'FRAME$', "", line)
-            print line_nr, len(line), len(frame)
-            if line_nr>=5: break
+                return
+            for i in range(1, len(words)):
+                key, value = self.pureNumInStr(words[i])
+                if not key: break
+                elif 'W'==key:
+                    if not self.levelPnt(3, value.group(0)):
+                        self.levelPnt(2, value.group(1))
+                    if not self._size:
+                        self._size = int(value.group(1))
+                    else: self._size *= int(value.group(1))
+                elif 'H'==key:
+                    if not self.levelPnt(3, value.group(0)):
+                        self.levelPnt(2, value.group(1))
+                    if not self._size:
+                        self._size = int(value.group(1))
+                    else: self._size *= int(value.group(1))
+                elif 'F'==key:
+                    if not self.levelPnt(3, value.group(0)):
+                        self.levelPnt(2, value.group(1))
+                elif 'A'==key:
+                    if not self.levelPnt(3, value.group(0)):
+                        self.levelPnt(2, value.group(1))
+                elif 'I'==key:
+                    if not self.levelPnt(3, value.group(0)):
+                        self.levelPnt(2, value.group(1))
+                else: self.levelPnt(1, value.group(0))
+            print "420 size=", self._size+self._size/2
+            print "422 size=", self._size*2
+            self._size += self._size/2
+            frames = 0
+            while True:
+                line = fr.readline()
+                frame=re.match(r'FRAME', line)
+                if frame:
+                    frames += 1
+                    self.levelPnt(2, frame.group(0)+' '+str(frames))
+                    fr.read(self._size)
+                else: break
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', type=int, action='store', dest='verbose', default=0,
+        choices=[0, 1, 2, 3])
+    parser.add_argument('-f', action='store',
+        dest='inFile', help='input file')
+    args = parser.parse_args()
+    obj = cY4M(args)
+    obj.parse()
 
